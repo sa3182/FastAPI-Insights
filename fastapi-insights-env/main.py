51a06b9799dd9339d61628784cffe1e4d59a2007
Shravani Athkuri <sa3182@rit.edu>
2024-04-08 23:22:35 -0400

    Creating blog api

--- fastapi-insights-env/main.py
+++ fastapi-insights-env/main.py
@@ -2,10 +2,22 @@ from fastapi import FastAPI
 
 app = FastAPI() #instance
 
+
+
 @app.get('/')
 def index():
-    return {"message": "Hello"}
+    return {'data':'blog list'}
 
 @app.get('/about')
 def about():
     return {'data': {'about': 'This is the information of the application'}}
+
+@app.get('/blog/{id}')
+def show(id):
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    #fetch comments of blog with id
+    return {'data': {'comments':{'Some comments'}}}
+
